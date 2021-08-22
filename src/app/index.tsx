fd7887bc5760d0f22ac66e1a5807602f20f3f745
Lin Lan <lanlin@meideng.net>
2021-08-22 14:41:16 +0800

    step2-1: 用路径画一个三角形

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -6,11 +6,11 @@ export function App(): JSX.Element {
   React.useEffect(() => {
     const ctx = cRef.current.getContext('2d');
 
-    ctx.fillStyle = 'red';
-    ctx.fillRect(10, 10, 50, 50);
-
-    ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
-    ctx.fillRect(30, 30, 50, 50);
+    ctx.beginPath();
+    ctx.moveTo(75, 50);
+    ctx.lineTo(100, 75);
+    ctx.lineTo(100, 25);
+    ctx.fill();
   }, []);
 
   return (
