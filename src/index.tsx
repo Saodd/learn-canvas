66197374db9c9b21faad15b71f2ae2eb5062dc11
Lin Lan <lanlin@meideng.net>
2021-08-22 11:48:11 +0800

    init project

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import { App } from './app';
+
+function main() {
+  ReactDOM.render(<App />, document.querySelector('#root'));
+}
+
+main();
