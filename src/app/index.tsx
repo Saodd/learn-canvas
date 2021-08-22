a8e9aa8a3775b63837d03c4d7bd734604a74ef6a
Lin Lan <lanlin@meideng.net>
2021-08-22 12:51:53 +0800

    step1-2: 画两个正方形

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,21 @@
 import * as React from 'react';
+import styles from './index.scss';
 
 export function App(): JSX.Element {
+  const cRef = React.useRef<HTMLCanvasElement>();
+  React.useEffect(() => {
+    const ctx = cRef.current.getContext('2d');
+
+    ctx.fillStyle = 'red';
+    ctx.fillRect(10, 10, 50, 50);
+
+    ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+    ctx.fillRect(30, 30, 50, 50);
+  }, []);
+
   return (
     <div>
-      <canvas width={800} height={800}>
+      <canvas width={800} height={800} ref={cRef} className={styles.myCanvas}>
         请使用最新的浏览器
       </canvas>
     </div>
