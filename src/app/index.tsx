b758054cd5f3c3c2a96598c2ab569f97130e4ab0
Lin Lan <lanlin@meideng.net>
2021-08-22 14:48:31 +0800

    step2-1: 用路径画一个笑脸

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -7,10 +7,20 @@ export function App(): JSX.Element {
     const ctx = cRef.current.getContext('2d');
 
     ctx.beginPath();
-    ctx.moveTo(75, 50);
-    ctx.lineTo(100, 75);
-    ctx.lineTo(100, 25);
+    ctx.moveTo(175, 150);
+    ctx.lineTo(200, 175);
+    ctx.lineTo(200, 125);
     ctx.fill();
+
+    ctx.beginPath();
+    ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // Outer circle
+    ctx.moveTo(110, 75);
+    ctx.arc(75, 75, 35, 0, Math.PI, false);  // Mouth (clockwise)
+    ctx.moveTo(65, 65);
+    ctx.arc(60, 65, 5, 0, Math.PI * 2, true);  // Left eye
+    ctx.moveTo(95, 65);
+    ctx.arc(90, 65, 5, 0, Math.PI * 2, true);  // Right eye
+    ctx.stroke();
   }, []);
 
   return (
