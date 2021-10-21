c018d343570a4a856ab3c93b2f4da652201589a3
Lin Lan <lanlin@meideng.net>
2021-10-21 16:37:00 +0800

    step6-1: 转换坐标系

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,56 +1,30 @@
 import * as React from 'react';
-import { useCallback, useEffect, useState } from 'react';
 import styles from './index.scss';
 
-const URL_BASE = 'https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Using_images/';
-const PICS: string[][] = [
-  ['gallery_1.jpg', 'gallery_2.jpg', 'gallery_3.jpg', 'gallery_4.jpg'],
-  ['gallery_5.jpg', 'gallery_6.jpg', 'gallery_7.jpg', 'gallery_8.jpg'],
-];
-
 export function App(): JSX.Element {
-  const [frame, setFrame] = useState<HTMLImageElement>(null);
-  useEffect(() => {
-    const img = new Image();
-    img.onload = () => {
-      setFrame(img);
-    };
-    img.src = URL_BASE + 'canvas_picture_frame.png';
-  }, []);
-  const draw = useCallback(
-    (e: React.SyntheticEvent) => {
-      const img = e.target as HTMLImageElement;
-      if (img.getAttribute('id') === 'frame') return;
-      const canvas = document.createElement('canvas');
-      // 将canvas设置为画框图片的尺寸
-      canvas.setAttribute('width', '132px');
-      canvas.setAttribute('height', '150px');
-      // 将canvas元素插入覆盖图片元素
-      img.parentNode.insertBefore(canvas, img);
+  const cRef = React.useRef<HTMLCanvasElement>();
+  React.useEffect(() => {
+    const ctx = cRef.current.getContext('2d');
 
-      // 绘制图片和画框
-      const ctx = canvas.getContext('2d');
-      ctx.drawImage(img, 15, 20);
-      ctx.drawImage(frame, 0, 0);
-    },
-    [frame],
-  );
+    ctx.fillStyle = 'red';
+    ctx.save();
+    for (let i = 0; i < 3; i++) {
+      ctx.translate(0, 50); // 坐标系相对向下移动50
+      ctx.save();
+      for (let j = 0; j < 3; j++) {
+        ctx.translate(50, 0); // 坐标系相对向右移动50
+        ctx.fillRect(0, 0, 25, 25);
+      }
+      ctx.restore();
+    }
+    ctx.restore();
+  }, []);
 
   return (
-    <div className={styles.body}>
-      {!!frame && (
-        <table>
-          {PICS.map((group, index) => (
-            <tr key={index}>
-              {group.map((pic) => (
-                <td key={pic}>
-                  <img src={URL_BASE+pic} onLoad={draw}/>
-                </td>
-              ))}
-            </tr>
-          ))}
-        </table>
-      )}
+    <div>
+      <canvas width={800} height={800} ref={cRef} className={styles.myCanvas}>
+        请使用最新的浏览器
+      </canvas>
     </div>
   );
 }
