e073ff5aa8e829bd5c29ccf4f06b787edf123dc1
Lin Lan <lanlin@meideng.net>
2021-10-22 15:00:09 +0800

    step7-1: 图像合成

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,24 +2,34 @@ import * as React from 'react';
 import styles from './index.scss';
 
 export function App(): JSX.Element {
-  const cRef = React.useRef<HTMLCanvasElement>();
+  const ref = React.useRef<HTMLDivElement>();
   React.useEffect(() => {
-    const ctx = cRef.current.getContext('2d');
+    const canvas0 = ref.current.children[0] as HTMLCanvasElement;
+    const ctx0 = canvas0.getContext('2d');
+    ctx0.fillStyle = 'red';
+    ctx0.fillRect(10, 10, 100, 100);
 
-    const sin = Math.sin(Math.PI / 6);
-    const cos = Math.cos(Math.PI / 6);
-    ctx.translate(100, 100);
-    for (let i = 0; i < 12; i++) {
-      const c = Math.floor(255 / 13 * i);
-      ctx.fillStyle = `rgb(${c},${c},${c})`;
-      ctx.fillRect(0, 0, 100, 10);
-      ctx.transform(cos, sin, -sin, cos, 0, 0);
-    }
+    const canvas1 = ref.current.children[1] as HTMLCanvasElement;
+    const ctx1 = canvas1.getContext('2d');
+    ctx1.fillStyle = 'green';
+    ctx1.fillRect(50, 50, 100, 100);
+
+    const canvas2 = ref.current.children[2] as HTMLCanvasElement;
+    const ctx2 = canvas2.getContext('2d');
+    ctx2.drawImage(canvas0, 0, 0);
+    ctx2.globalCompositeOperation = 'screen'; // 调整这里，观察效果
+    ctx2.drawImage(canvas1, 0, 0);
   }, []);
 
   return (
-    <div>
-      <canvas width={800} height={800} ref={cRef} className={styles.myCanvas}>
+    <div className={styles.body} ref={ref}>
+      <canvas width={200} height={200}>
+        请使用最新的浏览器
+      </canvas>
+      <canvas width={200} height={200}>
+        请使用最新的浏览器
+      </canvas>
+      <canvas width={200} height={200}>
         请使用最新的浏览器
       </canvas>
     </div>
